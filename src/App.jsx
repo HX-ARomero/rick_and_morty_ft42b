2429539df9c2670db4eb8515679d7ec87265d76e
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2023-09-08 16:08:27 -0300

    04-Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import './App.css';
-import Cards from './components/cards/Cards.jsx';
-import Nav from './components/nav/Nav';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import axios from "axios";
-
-import { Route, Routes } from 'react-router-dom';
 import About from './components/about/About';
+import Cards from './components/cards/Cards.jsx';
 import Detail from './components/detail/Detail';
+import Form from './components/form/Form';
+import Nav from './components/nav/Nav';
+import './App.css';
 
 function App() {
 
    const [ characters, setCharacters ] = useState([]); // [ estado, functión ]
    
+   const navigate = useNavigate(); // path actual y redirige!!!
+   // console.log(navigate);
+   const [access, setAccess] = useState(false);
+   const EMAIL = 'ejemplo@gmail.com';
+   const PASSWORD = '123456';
+
+   function login(userData) {
+      if (userData.password === PASSWORD && userData.email === EMAIL) {
+         setAccess(true);
+         navigate('/home');
+      } else {
+         alert("Por favor ingrese sus credenciales...")
+      }
+   }
+
+   useEffect(() => {
+      !access && navigate('/'); //! Ingresar a /home
+   }, [access]);
+
+   const location = useLocation();
+   console.log(location.pathname);
+
    function onSearch(id) {
       axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
          if (data.name) {
@@ -30,8 +52,16 @@ function App() {
 
    return (
       <div className='App'>
-         <Nav onSearch={onSearch} />
+         {
+            location.pathname !== "/" && <Nav onSearch={onSearch} />
+            // "/"   false => X
+            //  "/algoMas..." true => Evaluar la segunda parte
+         }
          <Routes >
+            <Route
+               path="/"
+               element={<Form login={login} />}
+            />
             <Route
                path="/home"
                element={<Cards characters={characters} onClose={onClose} />}
@@ -43,6 +73,10 @@ function App() {
             <Route
                path="/detail/:id"
                element={<Detail />}
+            />
+            <Route
+               path="*"
+               element={<About />}
             />
          </Routes>
          
