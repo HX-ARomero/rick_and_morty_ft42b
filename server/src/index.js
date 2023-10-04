94d41147065d56165560055c406ae01194f96536
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2023-10-04 13:17:21 -0300

    Sequelize I

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,11 @@
 const server = require("./app");
 const PORT = 3001;
+const { conn } = require('./DB_connection');
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+conn.sync({ force: true })
+   .then(() => {
+      server.listen(PORT, () => {
+         console.log('Server raised in port: ' + PORT);
+      });
+   })
+   .catch(error => console.log(error.message))
